4150eccb854782bd477a8aa31d9c050e0658e4be
dmillett <dbmillett@gmail.com>
2017-03-08 21:37:31 -0600

    Will store example data as edn for now.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :repl-options {:init (do
+                         (use 'political-canvas.shared.refdata.data)
+                         (use 'political-canvas.shared.tools))}
+  )
---
+++ src/political_canvas/shared/tools.clj
@@ -0,0 +1,12 @@
+(ns political-canvas.shared.tools)
+
+(defn data-to-file
+  "Dump a clojure data structure to an EDN file using (with-out-str). This
+  requires an existing directory or a filename inclusive of directory."
+  [data filename]
+  (spit (str filename ".edn") (with-out-str (pr data))) )
+
+(defn data-from-file
+  "Read the contents of a small file into its clojure data structure."
+  [filename]
+  (read-string (slurp filename)))
